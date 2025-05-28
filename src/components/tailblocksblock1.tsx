19c53cbdb83cd276b94e14dabb81a346764685cd
Lucas 2 <lucastoneatto2@gmail.com>
2025-05-28 20:17:24 +0000

    Install editor package

---
+++ src/components/tailblocksblock1.tsx
@@ -0,0 +1,3 @@
+export function tailblocksblock1(props: any) {
+  return <div>{props.t1}</div>;
+}
---
+++ src/components/tailblocksblock2.tsx
@@ -0,0 +1,3 @@
+export function tailblocksblock2(props: any) {
+  return <div>{props.t1}</div>;
+}
